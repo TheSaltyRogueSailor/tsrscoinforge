e872f6405bd4652717dc77cbd1b0fc2d1a4ea5fd
TheSaltyRogueSailor <raulgoodwin42@gmail.com>
2026-04-06 23:48:57 -0400

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,22 @@
 document.body.innerHTML = `
-<h1>TSRS Coin Forge 🚀</h1>
-<p>Frontend is LIVE</p>
+  <h1>TSRS Coin Forge 🚀</h1>
+  <p>Frontend is LIVE</p>
+
+  <button id="connectWallet">Connect Phantom Wallet</button>
+  <p id="walletAddress"></p>
 `
+
+const button = document.getElementById("connectWallet")!
+const walletText = document.getElementById("walletAddress")!
+
+button.onclick = async () => {
+  const provider = (window as any).solana
+
+  if (!provider || !provider.isPhantom) {
+    alert("Phantom Wallet not found. Install it.")
+    return
+  }
+
+  const resp = await provider.connect()
+  walletText.innerText = "Connected: " + resp.publicKey.toString()
+}
