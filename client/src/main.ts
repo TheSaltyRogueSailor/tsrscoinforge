51af1a3786f571dfa31823bcde87bab18c26341f
TheSaltyRogueSailor <raulgoodwin42@gmail.com>
2026-04-16 12:14:43 -0400

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,7 @@ import {
   LAMPORTS_PER_SOL
 } from "@solana/web3.js";
 
-const ALCHEMY_RPC_URL = "PASTE_YOUR_PUBLIC_ALCHEMY_RPC_URL_HERE";
+const ALCHEMY_RPC_URL = "https://solana-mainnet.g.alchemy.com/v2/VpKm0MUizuIShAsvvW2rJ";
 const FEE_WALLET = "9kkjHiAYFryfFVuWfBY9XuvrEVdCGZmWqhUnRGwreso8";
 const LAUNCH_FEE_SOL = 0.05;
 
