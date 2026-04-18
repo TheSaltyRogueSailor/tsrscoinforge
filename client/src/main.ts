fda4100b52ca1d3ab4e3f5c6739e632c99761e67
TheSaltyRogueSailor <raulgoodwin42@gmail.com>
2026-04-17 20:44:13 -0400

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -308,7 +308,7 @@ createBtn.onclick = async () => {
     resultSupply.innerText = "Supply: " + tokenSupply;
     resultDescription.innerText = "Description: " + tokenDescription;
     resultCA.innerText = "CA / Mint Address: " + mintData.mintAddress;
-    resultFeeTx.innerText = "Launch Fee Tx: " + feeSignature;
+    resultFeeTx.innerText = "Launch Fee Tx: " + feeSignature.slice(0, 12) + "..." + feeSignature.slice(-12);
    resultMintTx.innerText += "\nMetadata URL: " + (mintData.metadataUrl || "Not available");
 
     const solscanMint = `https://solscan.io/token/${mintData.mintAddress}`;
