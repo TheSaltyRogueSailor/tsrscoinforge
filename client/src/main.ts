073ccbc09e5dbce43deafbf2a65c72fa1b370896
TheSaltyRogueSailor <raulgoodwin42@gmail.com>
2026-04-19 23:26:19 -0400

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -30,7 +30,7 @@ document.body.innerHTML = `
   <button id="connectWallet" style="background:#2563eb; color:white; border:none; padding:12px 18px; border-radius:12px; font-weight:bold; font-size:16px; cursor:pointer; width:100%; max-width:340px;">      
             Connect Phantom Wallet
           </button>
-          <p id="walletAddress" style="margin:10px 0 0 0; color:#cbd5e1; font-size:14px;"></p>
+         <p id="walletAddress" style="margin:10px 0 0 0; color:#cbd5e1; font-size:14px; font-weight:600;"></p>
         </div>
       </div>
 
